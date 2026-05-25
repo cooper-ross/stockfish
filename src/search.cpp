efac275079cb8905f66d98f4a599b269329b5e8e
Cooper Ross <cooper.ross.c@gmail.com>
2026-05-25 16:29:51 -0400

    Add doEvenDeeperSearch tier to LMR fail-high re-search
    When the reduced LMR search of a late move returns far above the running
    bestValue, the current single-tier doDeeperSearch only grants +1 ply of
    extension. This patch adds a second tier so dramatically good late
    moves (value > bestValue + 142) are extended by 2 plies instead.
    
    Bench: 2940170

--- src/search.cpp
+++ src/search.cpp
@@ -1309,10 +1309,11 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = d < newDepth && value > bestValue + 52;
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doDeeperSearch     = d < newDepth && value > bestValue + 52;
+                const bool doEvenDeeperSearch = d < newDepth && value > bestValue + 142;
+                const bool doShallowerSearch  = value < bestValue + 9;
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += doDeeperSearch + doEvenDeeperSearch - doShallowerSearch;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
